676e5062602ae664f954fe5e7f46eef452475e15
Yev Barkalov <yevbar1729@gmail.com>
2025-08-03 20:20:25 -0300

    update port to switch request to

--- src/routes/browser.ts
+++ src/routes/browser.ts
@@ -25,7 +25,7 @@ browserRoutes.get("/title", async (c) => {
   // }
 
   const container = getContainer(c.env.BROWSER_CONTAINER);
-  return await container.fetch(switchPort(c.req.raw, 8081));
+  return await container.fetch(switchPort(c.req.raw, 3000));
 });
 
 export default browserRoutes;
