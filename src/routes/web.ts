b011b5b489b3f7f8fc71c25e30d7109f58eabcba
Yev Barkalov <yevbar1729@gmail.com>
2025-08-10 21:47:44 -0300

    update capitalization of letter F

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -13,7 +13,7 @@ webRoutes.get("/", (c) => {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agentflare</title>
+    <title>AgentFlare</title>
     <style>
         * {
             margin: 0;
@@ -223,7 +223,7 @@ webRoutes.get("/", (c) => {
 </head>
 <body>
     <div class="header">
-        <h1>Agentflare</h1>
+        <h1>AgentFlare</h1>
     </div>
     
     <div class="api-key-container">
